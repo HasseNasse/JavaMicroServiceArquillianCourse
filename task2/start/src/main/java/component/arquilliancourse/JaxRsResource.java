f112ce589cf597f6e34d7144279205d15e2190dc
Hassan Nazar <hnazar@redbridge.se>
2018-09-21 09:53:53 +0200

    removing old task2

--- task2/start/src/main/java/component/arquilliancourse/JaxRsResource.java
+++
@@ -1,8 +0,0 @@
-package component.arquilliancourse;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath( "/rest" )
-public class JaxRsResource extends Application {
-}
